6c7b298e141535f520fc30071f72e215aa80984d
Jasraj38 <jasrajdangi38@gmail.com>
2024-12-12 19:33:51 -0600

    Add files via upload

---
+++ Scripts/MainMenueManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+public class MainMenueManager : MonoBehaviour
+{
+
+
+    // fuction to start the gaame
+   public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    // function to exit the game
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
